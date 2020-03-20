0c3fd4a5ba7514314853ac34aae8f2566938d96f
Braslik <abraslik@gmail.com>
2020-03-20 16:27:03 +0200

    20.03.20

--- Web/React/react36/src/App.js
+++ Web/React/react36/src/App.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Welcome from "./ComponentsLesson2/Welcome";
-import Clock from "./ComponentsLesson2/Clock";
-const users = [{name:'Moshe'},{name:'Sara'},{name:'David'}]
+import './App.css'
+import Images from "./Homework/HW2_Components/Images";
+const parameters = [{height:'300'},{weight:'300'},{interval:'5000'}]
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return <div className='welcome'>
-    <Welcome user = {users[0].name}/>
-    <Welcome user = {users[1].name}/>
-    <Welcome user = {users[2].name}/>
+
+  return   <div className='Images'>
     <div>
-      <h3> It's time:</h3>
-    <Clock/>
+      <h1>“Images from the site https://loremflickr.com/”</h1>
+        <Images height = {parameters[0].height} weight = {parameters[1].weight} interval = {parameters[2].interval}/>
     </div>
   </div>
 }
