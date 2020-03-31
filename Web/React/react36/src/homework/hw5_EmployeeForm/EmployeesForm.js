18723b6a95b6beb9d40033092af8abc0beceff4e
Braslik <59307534+Braslik@users.noreply.github.com>
2020-03-31 08:54:44 +0300

    new

--- Web/React/react36/src/homework/hw5_EmployeeForm/EmployeesForm.js
+++ Web/React/react36/src/homework/hw5_EmployeeForm/EmployeesForm.js
@@ -21,38 +21,6 @@ export default class EmployeesForm extends React.Component {
        this.handlerInputSalary = this.handlerInputSalary.bind(this);
     }
 	
-	handleChange = (event) => {
-  event.preventDefault();
-  const { name, value } = event.target;
-  let errors = this.state.errors;
-
-  switch (name) {
-    case 'fullName': 
-      errors.fullName = 
-        value.length < 5
-          ? 'Full Name must be 5 characters long!'
-          : '';
-      break;
-    case 'email': 
-      errors.email = 
-        validEmailRegex.test(value)
-          ? ''
-          : 'Email is not valid!';
-      break;
-    case 'password': 
-      errors.password = 
-        value.length < 8
-          ? 'Password must be 8 characters long!'
-          : '';
-      break;
-    default:
-      break;
-  }
-
-  this.setState({errors, [name]: value}, ()=> {
-      console.log(errors)
-  })
-}
 
 
 handlerInputFields(event){
