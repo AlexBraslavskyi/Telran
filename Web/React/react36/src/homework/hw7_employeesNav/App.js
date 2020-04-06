706707bff8959b254d00492cba2a2707a692c4c6
Braslik <59307534+Braslik@users.noreply.github.com>
2020-04-06 07:49:10 +0300

    new

--- Web/React/react36/src/homework/hw7_employeesNav/App.js
+++ Web/React/react36/src/homework/hw7_employeesNav/App.js
@@ -1,1 +1,47 @@
+import React from 'react';
 
+import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import CoursesNav from "./components/CoursesNav";
+import {
+  PATH_COST_STATISTICS,
+  PATH_COURSES,
+  PATH_COURSES_GENERATION, PATH_COURSES_SEARCH,
+  PATH_TITLE_STATISTICS
+} from "./config/courses_configuration";
+import Courses from "./components/Courses";
+import CoursesGeneration from "./components/CoursesGeneration";
+import CostStatistics from "./components/CostStatistics";
+import TitleStatistics from "./components/TitleStatistics";
+import CoursesSearch from "./components/CoursesSearch";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    return <BrowserRouter>
+      <CoursesNav/>
+      <Switch>
+        <Route path={PATH_COURSES} exact render={() => {
+          return <Courses/>
+        }}/>
+        <Route path={PATH_TITLE_STATISTICS} exact render={() => {
+          return <TitleStatistics/>
+        }}/>
+        <Route path={PATH_COURSES_GENERATION} exact render={() => {
+          return <CoursesGeneration/>
+        }}/>
+        <Route path={PATH_COST_STATISTICS} exact render={() => {
+          return <CostStatistics/>
+        }}/>
+        <Route path={PATH_COURSES_SEARCH} exact render={() => {
+          return <CoursesSearch/>
+        }}/>
+
+
+      </Switch>
+    </BrowserRouter>
+  }
+}
+
+export default App;
