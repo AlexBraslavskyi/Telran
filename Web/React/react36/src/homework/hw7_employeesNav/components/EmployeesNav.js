b9b0442085db9b502e47f6185bd7858bb32456be
Braslik <59307534+Braslik@users.noreply.github.com>
2020-04-06 08:08:23 +0300

    new

--- Web/React/react36/src/homework/hw7_employeesNav/components/EmployeesNav.js
+++ Web/React/react36/src/homework/hw7_employeesNav/components/EmployeesNav.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {LINKS} from "../config/courses_configuration";
+import {LINKS} from "../config/employees_configuration";
 import {Link} from "react-router-dom";
-export default function CoursesNav(props) {
+export default function EmployeesNav(props) {
 const navItems = LINKS.map(link => {
     return <li key={link.path} className="nav-item">
         <Link to={link.path} className="nav-link">
