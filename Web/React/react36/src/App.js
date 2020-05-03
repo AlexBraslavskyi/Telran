0a0d26b6ad75304e27f48190cf3ed2d80b0e02f2
Braslik <abraslik@gmail.com>
2020-05-03 22:04:35 +0300

    Observ

--- Web/React/react36/src/App.js
+++ Web/React/react36/src/App.js
@@ -1,59 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './App.css';
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import {EmployeesNav} from "./homework/hw8_EmployeesHooks/Components/EmployeesNav";
+import Employees from "./homework/hw8_EmployeesHooks/Components/Employees";
+import TitleStatistics from "./homework/hw8_EmployeesHooks/Components/TitleStatistics";
+import {pathEmployees,pathTitleStatistics,pathSearch,pathSalaryStatistics,pathGenerations} from "./homework/hw8_EmployeesHooks/config/EmployeesConfig";
+import EmployeesGenerations from "./homework/hw8_EmployeesHooks/Components/EmployeesGenerations";
+import SalaryStatistics from "./homework/hw8_EmployeesHooks/Components/SalaryStatistics";
+import EmployeesSearch from "./homework/hw8_EmployeesHooks/Components/EmployeesSearch";
 
-import './homework/31-HW_EmployeesHooksWithBugs/App.css';
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
-import EmployeesNav from "./homework/31-HW_EmployeesHooksWithBugs/components/EmployeesNav";
-import {
-  PATH_EMPLOYEES,
-  PATH_GENERATION,
-  PATH_SALARY_STATISTICS, PATH_SEARCH,
-  PATH_TITLE_STATISTICS
-} from "./homework/31-HW_EmployeesHooksWithBugs/config/employees_config";
-import Employees from "./homework/31-HW_EmployeesHooksWithBugs/components/Employees";
-import TitleStatistics from "./homework/31-HW_EmployeesHooksWithBugs/components/TitleStatistics";
-import EmployeesGeneration from "./homework/31-HW_EmployeesHooksWithBugs/components/EmployeesGeneration";
-import SalaryStatistics from "./homework/31-HW_EmployeesHooksWithBugs/components/SalaryStatistics";
-import EmployeesSearch from "./homework/31-HW_EmployeesHooksWithBugs/components/EmployeesSearch";
+const App=(props)=>{
+ let employees;
+ let setEmployees;
+ [employees,setEmployees]=useState([]);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employees: []
-    }
-  }
-  updateEmployeesFn(employees) {
-    this.setState({
-      employees
-    })
-  }
-  render () {
+ const employeesUpdate = (employees) => {
+  setEmployees(employees)
+ }
     return <BrowserRouter>
-      <EmployeesNav/>
-      <Redirect to={PATH_GENERATION}/>
-      <Switch>
-        <Route path={PATH_EMPLOYEES} exact render={() => {
-          return <Employees employees={this.state.employees}
-          updateEmployeesFn={this.updateEmployeesFn.bind(this)}/>
-        }}/>
-        <Route path={PATH_TITLE_STATISTICS} exact render={() => {
-          return <TitleStatistics employees={this.state.employees}/>
-        }}/>
-        <Route path={PATH_GENERATION} exact render={() => {
-          return <EmployeesGeneration employees={this.state.employees}
-                                    updateEmployeesFn={this.updateEmployeesFn.bind(this)}/>
-        }}/>
-        <Route path={PATH_SALARY_STATISTICS} exact render={() => {
-          return <SalaryStatistics employees={this.state.employees}/>
-        }}/>
-        <Route path={PATH_SEARCH} exact render={() => {
-          return <EmployeesSearch employees={this.state.employees}/>
-        }}/>
-
-
-      </Switch>
-    </BrowserRouter>
-  }
-}
-
+    <EmployeesNav></EmployeesNav>
+    <Switch>
+      <Route path={pathEmployees} exact render ={() =>
+        {return <Employees employees = {employees} updateEmployeesFn={employeesUpdate}/>}}/>
+      <Route path={pathTitleStatistics} exact render={() =>
+        {return <TitleStatistics employees={employees}/>}}/>
+      <Route path={pathGenerations} exact render={() =>
+        {return <EmployeesGenerations  employees = {employees} updateEmployeesFn={employeesUpdate}/>}}/>
+      <Route path={pathSalaryStatistics} exact render={() =>
+        {return <SalaryStatistics employees={employees}/>}}/>
+        <Route path={pathSearch} exact render={() =>
+        {return <EmployeesSearch employees={employees}/>}}/>
+              </Switch>
+              </BrowserRouter>
+      }
 export default App;
