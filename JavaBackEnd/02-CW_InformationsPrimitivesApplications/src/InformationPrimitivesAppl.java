67c45dc5bbbfd5142c36b3948cfaf777018cc117
Alexander <abraslik@gmail.com>
2020-06-11 07:09:06 +0300

    Update primitives

--- JavaBackEnd/02-CW_InformationsPrimitivesApplications/src/InformationPrimitivesAppl.java
+++ JavaBackEnd/02-CW_InformationsPrimitivesApplications/src/InformationPrimitivesAppl.java
@@ -16,13 +16,25 @@ public class InformationPrimitivesAppl {
 			String result = getInformation(primitiveType, informationType);
 			System.out.println(result);
 		}
-
 	}
+
 
 	private static String getInformation(String primitiveType, String informationType) {
 		switch (primitiveType) {
-		case "int": return getIntInformation(informationType);
-//		TODO 7cases for all types
+		case "int":
+				return getIntInformation(informationType);
+		case "long":
+			return getLongInformation(informationType);
+		case "char":
+			return getCharInformation(informationType);
+		case "byte":
+			return getByteInformation(informationType);
+		case "double":
+			return getDoubleInformation(informationType);
+		case "float":
+			return getFloatInformation(informationType);
+		case "short":
+			return getShortInformation(informationType);
 				default:return "Error wrong primitive type";
 		}
 		
@@ -31,7 +43,62 @@ public class InformationPrimitivesAppl {
 	private static String getIntInformation(String informationType) {
 		switch(informationType) {
 		case "min": return ""+Integer.MIN_VALUE;
-//		TODO 3
+		case "max": return ""+Integer.MAX_VALUE;
+		case "bytes": return ""+Integer.BYTES;
+		default: return "Error: wrong information type";
+	}
+
+}
+	private static String getLongInformation(String informationType) {
+		switch(informationType) {
+		case "min": return ""+Long.MIN_VALUE;
+		case "max": return ""+Long.MAX_VALUE;
+		case "bytes": return ""+Long.BYTES;
+		default: return "Error: wrong information type";
+	}
+
+}
+	private static String getByteInformation(String informationType) {
+		switch(informationType) {
+		case "min": return ""+Byte.MIN_VALUE;
+		case "max": return ""+Byte.MAX_VALUE;
+		case "bytes": return ""+Byte.BYTES;
+		default: return "Error: wrong information type";
+	}
+
+}
+	private static String getCharInformation(String informationType) {
+		switch(informationType) {
+		case "min": return ""+Character.MIN_VALUE;
+		case "max": return ""+Character.MAX_VALUE;
+		case "bytes": return ""+Character.BYTES;
+		default: return "Error: wrong information type";
+	}
+
+}
+	private static String getDoubleInformation(String informationType) {
+		switch(informationType) {
+		case "min": return ""+Double.MIN_VALUE;
+		case "max": return ""+Double.MAX_VALUE;
+		case "bytes": return ""+Double.BYTES;
+		default: return "Error: wrong information type";
+	}
+
+}
+	private static String getShortInformation(String informationType) {
+		switch(informationType) {
+		case "min": return ""+Short.MIN_VALUE;
+		case "max": return ""+Short.MAX_VALUE;
+		case "bytes": return ""+Short.BYTES;
+		default: return "Error: wrong information type";
+	}
+
+}
+	private static String getFloatInformation(String informationType) {
+		switch(informationType) {
+		case "min": return ""+Float.MIN_VALUE;
+		case "max": return ""+Float.MAX_VALUE;
+		case "bytes": return ""+Float.BYTES;
 		default: return "Error: wrong information type";
 	}
 
