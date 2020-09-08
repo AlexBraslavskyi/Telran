65d0a123800be93cb0309503a64880eb2d67baf6
Alexander <abraslik@gmail.com>
2020-09-08 07:06:51 +0300

    Update

--- JavaBackEnd/27-CW_BracketsChecker/src/telran/expressions/BracketsChecker.java
+++ JavaBackEnd/27-CW_BracketsChecker/src/telran/expressions/BracketsChecker.java
@@ -7,40 +7,40 @@ import java.util.Stack;
 import java.util.TreeSet;
 
 public class BracketsChecker {
-	
-	HashMap <String, String> closeOpenBrackets = new HashMap<>();
-	String [][] bracketsData;
+
+	HashMap<String, String> closeOpenBrackets = new HashMap<>();
+	String[][] bracketsData;
 	Set<String> openBrackets = new TreeSet<>();
-	public BracketsChecker(String [][] bracketsData) {
+
+	public BracketsChecker(String[][] bracketsData) {
 		this.bracketsData = bracketsData;
-	for(String[] brackets:bracketsData) {
-		closeOpenBrackets.put(brackets[1], brackets[0]);
-		openBrackets.add(brackets[0]);
+		for (String[] brackets : bracketsData) {
+			closeOpenBrackets.put(brackets[1], brackets[0]);
+			openBrackets.add(brackets[0]);
+		}
+
 	}
-		
-	}
-	
-	
-	
+
 	public boolean bracketsCheck(String expression) {
-		
-		String [] brackets = getBrackets(expression);
-		
+		System.out.println(expression);
+		String[] brackets = getBrackets(expression);
+
 		return check(brackets);
 	}
+
 	private boolean check(String[] brackets) {
 
 		Stack<String> stackBrackets = new Stack<>();
-		for(String bracket:brackets) {
-			if(openBrackets.contains(bracket)) {
+		for (String bracket : brackets) {
+			if (openBrackets.contains(bracket)) {
 				stackBrackets.push(bracket);
-			}else {
-				if(stackBrackets.isEmpty()) {
+			} else {
+				if (stackBrackets.isEmpty()) {
 					return false;
 				}
-				if(closeOpenBrackets.get(bracket).equals(stackBrackets.peek())) {
+				if (closeOpenBrackets.get(bracket).equals(stackBrackets.peek())) {
 					stackBrackets.pop();
-				}else {
+				} else {
 					return false;
 				}
 			}
@@ -48,44 +48,108 @@ public class BracketsChecker {
 		return stackBrackets.isEmpty();
 	}
 
-
-
 	private String[] getBrackets(String expression) {
-		ArrayList <String> res = new ArrayList<>();
-		for(String [] brackets:bracketsData) {
-			for(String brack:brackets) {
-		if(expression.contains(brack)) {
-			res.add(brack);
-		}
-//		for(String brack:bracketsData[1]) {
-//			if(expression.contains(brack)) {
-//				res.add(brack);
+//		TreeSet <Character> treeOfChars = new TreeSet<>();
+//		for(String brackets[]:bracketsData) {
+//			for(String bracket:brackets) {
+//				for (char a:bracket.toCharArray()) {
+//					treeOfChars.add(a);
+//				}
 //			}
+//		}
+//		
+//			System.out.println(treeOfChars);
+
+//		    char[] chars = prefix.toCharArray();
+//		    if(chars.length>0) {
+//		        chars[chars.length-1] = (char) (chars[chars.length-1]+1);
+//		    }
+//		}
+//		return treeOfWords.subSet(prefix, true, chars.toString(), true).toArray(new String[0]);
+//		return null;
+
+		ArrayList<Character> exp = new ArrayList<Character>();
+		for (char a : expression.toCharArray()) {
+			exp.add(a);
 		}
+		ArrayList<String> res = new ArrayList<>();
+		String temp = "";
+		for (String brackets[] : bracketsData) {
+			for (String bracket : brackets) {
+				char[] bracketChar = bracket.toCharArray();
+				for (int i = 0; i < bracketChar.length; i++) {
+					if (bracketChar.length == 1 && exp.contains(bracketChar[i])) {
+						res.add(bracketChar[i] + "");
+						exp.remove(exp.indexOf(bracketChar[i]));
+					} else if (exp.contains(bracketChar[i])) {
+						temp = temp + bracketChar[i];
+						exp.remove(exp.indexOf(bracketChar[i]));
+//						break;
+					} else if (bracketChar.length == 1 && exp.contains(bracketChar[i])) {
+						temp = temp + bracketChar[i];
+						exp.remove(exp.indexOf(bracketChar[i]));
+						if (i == bracketChar.length - 1) {
+							res.add(temp);
+							temp = "";
+						}
+					}
+//		char[] exp = expression.toCharArray();
+//		
+////		System.out.println(exp);
+//		ArrayList <String> res = new ArrayList<>();
+//
+//		String temp = "";
+//		for(String brackets[]:bracketsData) {
+//			for(String bracket:brackets) {
+//			char []bracketChar = bracket.toCharArray();
+////			System.out.print(bracketChar);
+//for(int i = 0, j = 0; i < exp.length;i++) {
+//	if(bracketChar.length == 1 && exp[j]==bracketChar[i]) {
+//		 res.add(bracketChar[i]+"");
+//		 exp[j] = 0;
+//		 System.out.println(exp);
+//		 break;
+//		 
+//	}else if(exp[j] == bracketChar[i]){
+//		 temp = temp + bracketChar[i];
+//		 exp[j] = 0;
+//		 break;
+//		 }
+//if(exp[j] == bracketChar[i]&&i <= bracketChar.length-2 && exp[j+1] != bracketChar[i+1]||i == bracketChar.length-1) {
+////	System.out.println(temp);
+//	res.add(temp);
+//	temp = "";
+//	exp[j] = 0;
+//}
+//if(i == bracketChar.length-1) {
+//	i = 0;
+
+				}
+
+			}
 		}
-		res.forEach(System.out::println);
-		return (String[]) res.toArray();
+		System.out.println();
+		res.forEach(System.out::print);
+		System.out.println();
+		System.out.println();
+		return res.toArray(new String[0]);
+
 	}
 
-
-	//()
+	// ()
 	public static boolean parenthessesCheck(String expression) {
 		int count = 0;
-		for(char sym:expression.toCharArray()) {
-			if(sym =='(') {
+		for (char sym : expression.toCharArray()) {
+			if (sym == '(') {
 				count++;
-			}else if(sym ==')') {
+			} else if (sym == ')') {
 				count--;
-				if(count < 0) {
+				if (count < 0) {
 					return false;
 				}
 			}
 		}
 		return count == 0;
 	}
-	
-	
-	
-	
 
 }
--- JavaBackEnd/27-CW_BracketsChecker/src/telran/tests/BracketsCheckerTest.java
+++ JavaBackEnd/27-CW_BracketsChecker/src/telran/tests/BracketsCheckerTest.java
@@ -1,40 +1,68 @@
 package telran.tests;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static telran.expressions.BracketsChecker.parenthessesCheck;
 
 import org.junit.jupiter.api.Test;
 
 import telran.expressions.BracketsChecker;
 
 class BracketsCheckerTest {
-String [][] brackets = {{"{","}"},{"<table>","</table>"},{"[","]"},{"/*","*/"}};
-BracketsChecker bracketsChecker = new BracketsChecker(brackets);
-	@Test
-	void testParenthessesTrue() {
-	assertTrue(parenthessesCheck("abc(uui)"));
-	assertTrue(parenthessesCheck("()hhhh"));
-	assertTrue(parenthessesCheck("a(bc())hhhh()"));
-	assertTrue(parenthessesCheck("((()))"));
-	}
-	@Test
-	void testParenthessesFalse() {
-		assertFalse(parenthessesCheck("abc(uui))"));
-		assertFalse(parenthessesCheck("abcuui))"));
-		assertFalse(parenthessesCheck("abc(uui))"));
-		assertFalse(parenthessesCheck("ab(c(()"));
-		assertFalse(parenthessesCheck(")("));
-		assertFalse(parenthessesCheck("(()))("));
-	}
-	@Test
-	void testBracketsTrue() {
-	assertTrue(bracketsChecker.bracketsCheck("{qqq}{}{}"));
-	assertTrue(bracketsChecker.bracketsCheck("<table>[/*r*/]r{r}r</table>"));
-	assertTrue(bracketsChecker.bracketsCheck("aaaa<table>[/*r*/r{r}r]</table>"));
-	}
-	@Test
-	void testBracketsFalse() {
-		assertFalse(bracketsChecker.bracketsCheck("{qqq{}{}"));
-		assertFalse(bracketsChecker.bracketsCheck("<table>/*r*/r{r}r]table>"));
-	}
+	String [][]brackets = {{"{", "}"},{"<table>", "</table>"},{"[","]"},{"/*", "*/"}, {"<", ">"}};
+	BracketsChecker bracketsChecker = new BracketsChecker(brackets);
+//		@Test
+//		void testParenthesesTrue() {
+//			assertTrue(parenthessesCheck("abcc(hhhh)"));
+//			assertTrue(parenthessesCheck("(((())))"));
+//			assertTrue(parenthessesCheck("((c(ab(lm))))"));
+//		}
+//		@Test
+//		void testParenthesesFalse() {
+//			assertFalse(parenthessesCheck("abcc(hh(hh)"));
+//			assertFalse(parenthessesCheck("(((()))"));
+//			assertFalse(parenthessesCheck("((c(ab)(lm))))"));
+//			assertFalse(parenthessesCheck(")("));
+//		}
+		@Test 
+		void testBracketsTrue() {
+			assertTrue(bracketsChecker.bracketsCheck("<table>[(hhhh]</table>"));
+			assertTrue(bracketsChecker.bracketsCheck("<table>aaaaaa[(hhhh]</table>"));
+			assertTrue(bracketsChecker.bracketsCheck("<table>[{hhhh}]</table>"));
+			assertTrue(bracketsChecker.bracketsCheck("/*{uuuu}[{;;;;;}]*/"));
+			assertTrue(bracketsChecker.bracketsCheck("<<table>[{hhhh}]</table>>"));
+		}
+		@Test 
+		void testBracketsFalse() {
+			assertFalse(bracketsChecker.bracketsCheck("<table>[{hhhh]</table>"));
+			assertFalse(bracketsChecker.bracketsCheck("<table>[{hhhh)]</table>"));
+			assertFalse(bracketsChecker.bracketsCheck("/*{uu*/uu}[{;;;;;}]*/"));
+		}
+//String [][] brackets = {{"{","}"},{"<table>","</table>"},{"[","]"},{"/*","*/"}};
+//BracketsChecker bracketsChecker = new BracketsChecker(brackets);
+//	@Test
+//	void testParenthessesTrue() {
+//	assertTrue(parenthessesCheck("abc(uui)"));
+//	assertTrue(parenthessesCheck("()hhhh"));
+//	assertTrue(parenthessesCheck("a(bc())hhhh()"));
+//	assertTrue(parenthessesCheck("((()))"));
+//	}
+//	@Test
+//	void testParenthessesFalse() {
+//		assertFalse(parenthessesCheck("abc(uui))"));
+//		assertFalse(parenthessesCheck("abcuui))"));
+//		assertFalse(parenthessesCheck("abc(uui))"));
+//		assertFalse(parenthessesCheck("ab(c(()"));
+//		assertFalse(parenthessesCheck(")("));
+//		assertFalse(parenthessesCheck("(()))("));
+//	}
+//	@Test
+//	void testBracketsTrue() {
+//	assertTrue(bracketsChecker.bracketsCheck("{qqq}{}{}"));
+//	assertTrue(bracketsChecker.bracketsCheck("<table>[/*r*/]r{r}r</table>"));
+//	assertTrue(bracketsChecker.bracketsCheck("aaaa<table>[/*r*/r{r}r]</table>"));
+//	}
+//	@Test
+//	void testBracketsFalse() {
+//		assertFalse(bracketsChecker.bracketsCheck("{qqq{}{}"));
+//		assertFalse(bracketsChecker.bracketsCheck("<table>/*r*/r{r}r]table>"));
+//	}
 }
