1e81df3e8b2a6294e64772eb50990de5d5bba5f5
Alexander <abraslik@gmail.com>
2021-03-15 06:48:10 +0200

    Add DiscoveryLoadBalancer

---
+++ JavaBackEnd/discovery-loadbalancer-client/src/test/java/discovery/microservices/DiscoveryLoadbalancerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package discovery.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryLoadbalancerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
